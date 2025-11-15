4af7510e4d3f7af19c8999fc3c7e3e23c7e2976b
BackEnd-Studio <b4ck3nd03@gmail.com>
2025-11-15 20:04:41 +0700

    Fix import path for react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useMemo } from 'react';
 // --- Impor Ikon ---
-import { FaWhatsapp, FaTiktok } from 'react-icons/fa6';
+import { FaWhatsapp, FaTiktok } from 'react-icons/fa';
 import { BsThreads } from 'react-icons/bs'; // Ikon Threads dari Bootstrap
 
 // --- (Impor Firebase tidak kita pakai di proyek ini) ---
